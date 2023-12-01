3ee9d460c25bdc4f4289120968b7755decae4750
svenhofman <svenhofman25@gmail.com>
2023-12-01 17:05:12 +0100

    removed some commented code

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -7,20 +7,6 @@ function ScoreBoard({ numGuesses }) {
                 <div className='difficulty easy'>{numGuesses['easy'] < Infinity ? numGuesses['easy'] : '-'} (easy)</div>
                 <div className='difficulty medium'>{numGuesses['medium'] < Infinity ? numGuesses['medium'] : '-'} (medium)</div>
                 <div className='difficulty hard'>{numGuesses['hard'] < Infinity ? numGuesses['hard'] : '-'} (hard)</div>
-                {/*<div className='scores'>*/}
-                {/*    <div className='difficulty easy'>*/}
-                {/*        <div className='label'>easy</div>*/}
-                {/*        <div className='score'>0</div>*/}
-                {/*    </div>*/}
-                {/*    <div className='difficulty medium'>*/}
-                {/*        <div className='label'>medium</div>*/}
-                {/*        <div className='score'>0</div>*/}
-                {/*    </div>*/}
-                {/*    <div className='difficulty hard'>*/}
-                {/*        <div className='label'>hard</div>*/}
-                {/*        <div className='score'>0</div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
             </div>
         </div>
     );
