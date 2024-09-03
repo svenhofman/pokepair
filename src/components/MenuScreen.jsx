81f18d6a32869c8174197e2fca1ec9ef4b58c5a5
svenhofman <svenhofman25@gmail.com>
2024-09-03 10:24:33 +0200

    added github link

--- src/components/MenuScreen.jsx
+++ src/components/MenuScreen.jsx
@@ -1,19 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 function MenuScreen({ refCSSTransition, difficulty, setDifficulty, startGame }) {
     return (
-        <div ref={refCSSTransition} className='modal menu'>
-            <div className='difficulty-options'>
-                <div className={`easy ${difficulty === 'easy' ? 'selected' : ''}`} onClick={() => setDifficulty('easy')}>
-                    easy
+        <div className='menu-screen' ref={refCSSTransition} >
+            <a  className='github-link' href='https://github.com/svenhofman/pokepair' target='_blank' rel='noopener noreferrer'>
+                <FontAwesomeIcon icon={faGithub} className='github-icon' />
+            </a>
+            <div className='modal menu'>
+                <div className='difficulty-options'>
+                    <div className={`easy ${difficulty === 'easy' ? 'selected' : ''}`} onClick={() => setDifficulty('easy')}>
+                        easy
+                    </div>
+                    <div className={`medium ${difficulty === 'medium' ? 'selected' : ''}`} onClick={() => setDifficulty('medium')}>
+                        medium
+                    </div>
+                    <div className={`hard ${difficulty === 'hard' ? 'selected' : ''}`} onClick={() => setDifficulty('hard')}>
+                        hard
+                    </div>
                 </div>
-                <div className={`medium ${difficulty === 'medium' ? 'selected' : ''}`} onClick={() => setDifficulty('medium')}>
-                    medium
+                <div className='start' onClick={startGame}>
+                    start
                 </div>
-                <div className={`hard ${difficulty === 'hard' ? 'selected' : ''}`} onClick={() => setDifficulty('hard')}>
-                    hard
-                </div>
-            </div>
-            <div className='start' onClick={startGame}>
-                start
             </div>
         </div>
     );
